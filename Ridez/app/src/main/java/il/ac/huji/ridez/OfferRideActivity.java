4f23f204bee33d97fa4d7974c03333347cc5f0b3
Tzvi Corn <tzvi.corn@mail.huji.ac.il>
2015-08-05 11:01:48 +0300

    auto complete in google now works

---
+++ Ridez/app/src/main/java/il/ac/huji/ridez/OfferRideActivity.java
@@ -0,0 +1,307 @@
+package il.ac.huji.ridez;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.NumberPicker;
+import java.util.Calendar;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.widget.NumberPicker.OnValueChangeListener;
+
+import java.util.Date;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.Toast;
+
+
+public class OfferRideActivity extends ActionBarActivity {
+    EditText origin;
+    EditText destination;
+    int numOfPassengers = 0;
+    final Calendar c = Calendar.getInstance();
+    int mYear = c.get(Calendar.YEAR);
+    int mMonth = c.get(Calendar.MONTH);
+    int mDay = c.get(Calendar.DAY_OF_MONTH);
+    int mHour = c.get(Calendar.HOUR_OF_DAY);
+    int mMinute = c.get(Calendar.MINUTE);
+
+    Button saveRequestButton;
+    Button setDateButton;
+    Button setTimeButton;
+    Button  setAmountButton;
+
+    TextView dateTextView;
+    TextView timeTextView;
+    int ourYear;
+    int ourMonth;
+    int ourDay;
+    int ourHour;
+    int ourMinute;
+    ListView groupsListView;
+    NumberPicker np;
+    private static final String LOG_TAG = "Places Autocomplete";
+    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+    private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+    private static final String OUT_JSON = "/json";
+
+    private static final String API_KEY = "AIzaSyBjx-Mb6MzVQuef6E-30u-gEa0wuuK3sr4";
+
+    public static ArrayList autocomplete(String input) {
+        ArrayList resultList = null;
+
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+        try {
+            StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+            sb.append("?key=" + API_KEY);
+            //sb.append("&components=country:gr");
+            sb.append("&input=" + URLEncoder.encode(input, "utf8"));
+
+            URL url = new URL(sb.toString());
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Load the results into a StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with Places API URL", e);
+            return resultList;
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "connection err PlacesAPI", e);
+            return resultList;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        try {
+            // Create a JSON object hierarchy from the results
+            JSONObject jsonObj = new JSONObject(jsonResults.toString());
+            JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+
+            // Extract the Place descriptions from the results
+            resultList = new ArrayList(predsJsonArray.length());
+            for (int i = 0; i < predsJsonArray.length(); i++) {
+                System.out.println(predsJsonArray.getJSONObject(i).getString("description"));
+                System.out.println("============================================================");
+                resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Cannot process JSON results", e);
+        }
+
+        return resultList;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_ride);
+        //origin = (EditText) findViewById(R.id.originOffering);
+        //destination = (EditText) findViewById(R.id.destinationOffering);
+        final AutoCompleteTextView autoCompView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        autoCompView.setAdapter(new GooglePlacesAutocompleteAdapter(this, R.layout.list_item));
+        autoCompView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int position, long id) {
+                String str = (String) adapterView.getItemAtPosition(position);
+                Toast.makeText(OfferRideActivity.this, str, Toast.LENGTH_SHORT).show();
+                autoCompView.setText(str);
+            }
+        });
+        final AutoCompleteTextView autoCompViewOrigin = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewOrigin);
+        autoCompViewOrigin.setAdapter(new GooglePlacesAutocompleteAdapter(this, R.layout.list_item));
+        autoCompViewOrigin.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int position, long id) {
+                String str = (String) adapterView.getItemAtPosition(position);
+                Toast.makeText(OfferRideActivity.this, str, Toast.LENGTH_SHORT).show();
+                autoCompViewOrigin.setText(str);
+            }
+        });
+        dateTextView = (TextView)findViewById(R.id.dateTextViewOffering);
+        timeTextView = (TextView) findViewById(R.id.timeTextViewOffering);
+
+
+
+
+        np = (NumberPicker) findViewById(R.id.amountNumberPickerOffering);
+        np.setMinValue(1);
+        np.setMaxValue(5);
+        np.setValue(1);
+        setDateButton = (Button) findViewById(R.id.dateButtonOffering);
+        setDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dpd = new DatePickerDialog(OfferRideActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                ourYear = year;
+                                ourMonth = monthOfYear;
+                                ourDay = dayOfMonth;
+                                dateTextView.setText(new StringBuilder()
+                                        // Month is 0 based, just add 1
+                                        .append(ourDay).append("-").append(ourMonth + 1).append("-")
+                                        .append(ourYear).append(" "));
+                            }
+                        }, mYear, mMonth, mDay);
+                dpd.show();
+            }
+        });
+        setTimeButton = (Button) findViewById(R.id.timeButtonOffering);
+        setTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog tpd = new TimePickerDialog(OfferRideActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+                                ourHour = hourOfDay;
+                                ourMinute = minute;
+                                timeTextView.setText(new StringBuilder()
+                                        // Month is 0 based, just add 1
+                                        .append(ourHour).append(":").append(ourMinute));
+                            }
+                        }, mHour, mMinute, false);
+                tpd.show();
+            }
+        });
+
+
+        saveRequestButton = (Button) findViewById(R.id.saveRequestButtonOffering);
+        saveRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check if date is not null, all other fields valid
+                //and then register the request in parse server;
+                Calendar cal = Calendar.getInstance();
+                cal.setTimeInMillis(0);
+                cal.set(ourYear, ourMonth, ourDay, ourHour, ourMinute, 0);
+                Date date = cal.getTime();
+
+                //create request on server
+                //save to db
+                Intent requestDetails = new Intent(OfferRideActivity.this, RequestDetails.class);
+                requestDetails.putExtra("origin", origin.getText().toString());
+                requestDetails.putExtra("destination", destination.getText().toString());
+                requestDetails.putExtra("date", date.getTime());
+                requestDetails.putExtra("amount", np.getValue());
+                OfferRideActivity.this.startActivity(requestDetails);
+                OfferRideActivity.this.finish();
+
+            }
+        });
+        groupsListView = (ListView) findViewById(R.id.groupListView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_offer_ride, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class GooglePlacesAutocompleteAdapter extends ArrayAdapter implements Filterable {
+        private ArrayList resultList;
+
+        public GooglePlacesAutocompleteAdapter(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+        }
+
+        @Override
+        public int getCount() {
+            return resultList.size();
+        }
+
+        @Override
+        public String getItem(int index) {
+            return resultList.get(index).toString();
+        }
+
+        @Override
+        public Filter getFilter() {
+            Filter filter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults filterResults = new FilterResults();
+                    if (constraint != null) {
+                        // Retrieve the autocomplete results.
+                        resultList = autocomplete(constraint.toString());
+
+                        // Assign the data to the FilterResults
+                        filterResults.values = resultList;
+                        filterResults.count = resultList.size();
+                    }
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    if (results != null && results.count > 0) {
+                        notifyDataSetChanged();
+                    } else {
+                        notifyDataSetInvalidated();
+                    }
+                }
+            };
+            return filter;
+        }
+    }
+}
