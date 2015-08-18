af29e3611a88654cfa88bad2f7ff5554a0759f39
Tzvi Corn <tzvi.corn@mail.huji.ac.il>
2015-08-18 19:26:30 +0300

    in the middle of group details screen

---
+++ Ridez/app/src/main/java/il/ac/huji/ridez/GroupMembersFragment.java
@@ -0,0 +1,44 @@
+package il.ac.huji.ridez;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import il.ac.huji.ridez.R;
+import il.ac.huji.ridez.adpaters.RidezAdapter;
+import il.ac.huji.ridez.sqlHelpers.GroupInfo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class GroupMembersFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_group_members, container, false);
+        return rootView;
+    }
+}
---
+++ Ridez/app/src/main/java/il/ac/huji/ridez/adpaters/groupDetailsTabAdapter.java
@@ -0,0 +1,42 @@
+package il.ac.huji.ridez.adpaters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import il.ac.huji.ridez.FutureRides;
+import il.ac.huji.ridez.GroupMembersFragment;
+import il.ac.huji.ridez.GroupRidesFragment;
+import il.ac.huji.ridez.PastRidesFragment;
+
+/**
+ * Created by Tzvi on 18/08/2015.
+ */
+public class groupDetailsTabAdapter extends FragmentPagerAdapter {
+    int groupIndex;
+    public groupDetailsTabAdapter(FragmentManager fm, int index) {
+        super(fm);
+        this.groupIndex = index;
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                // Top Rated fragment activity
+                return new GroupMembersFragment(groupIndex);
+            case 1:
+                // Games fragment activity
+                return new GroupRidesFragment(/*groupIndex*/);
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 2;
+    }
+}
